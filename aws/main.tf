99ecfe86ceeff14caaa70035d13fdcf88dceb125
Slawek Zachcial <slawek.zachcial@hp.com>
2022-04-04 18:53:04 +0200

    Add terraform to create AWS resources

---
+++ aws/main.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+resource "aws_iam_openid_connect_provider" "this" {
+  url  = var.oidc_issuer_base_url
+  tags = var.tags
+
+  client_id_list = [
+    "aws_spa"
+  ]
+
+  thumbprint_list = [
+    lower(var.oidc_issuer_thumbprint)
+  ]
+}
+
+resource "aws_iam_role" "this" {
+  name = "aws_spa_oauth"
+  tags = var.tags
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : {
+        "Effect" : "Allow",
+        "Principal" : { "Federated" : aws_iam_openid_connect_provider.this.arn },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "${replace(var.oidc_issuer_base_url, "https://", "")}:aud" : var.oidc_client_id
+          }
+        }
+      }
+    }
+  )
+
+  inline_policy {
+    name = "aws_spa_oauth"
+
+    policy = jsonencode(
+      {
+        "Version" : "2012-10-17",
+        "Statement" : [
+          {
+            "Effect" : "Allow",
+            "Action" : [
+              "sts:GetCallerIdentity"
+            ],
+            "Resource" : "*"
+          }
+        ]
+      }
+    )
+  }
+}
---
+++ aws/variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "aws_profile" {
+  default = "personal"
+}
+
+variable "oidc_issuer_base_url" {
+  default = "https://localhost/auth/realms/master"
+}
+
+variable "oidc_issuer_thumbprint" {
+  # To get thumbprint: openssl x509 -in ../keycloak/cert.pem -fingerprint -noout | sed -e 's/.*=//' -e 's/://g'
+  type = string
+}
+
+variable "oidc_client_id" {
+  default = "aws-personal"
+}
+
+variable "tags" {
+  default = {
+    Project = "aws-spa-oauth"
+  }
+}
