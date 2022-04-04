9871b668e3c91d6d809b186e28de8f33e56548c9
Slawek Zachcial <slawek.zachcial@hp.com>
2022-04-04 15:11:20 +0200

    Rework spa app javascript

--- spa/public/app.js
+++ spa/public/app.js
@@ -1,12 +1,14 @@
-document.getElementById('login').addEventListener("click", login, false);
-document.getElementById('logout').addEventListener("click", logout, false);
-
 Oidc.Log.logger = console;
 Oidc.Log.level = Oidc.Log.DEBUG;
 
 console.log(`Using oidc-client version: ${Oidc.Version}`);
 
-function greetUser(user) {
+const mgr = new Oidc.UserManager(oidcConfig);
+
+const login = () => mgr.signinRedirect();
+const logout = () => mgr.signoutRedirect();
+
+const greetUser = (user) => {
     console.log(JSON.stringify(user));
     const greeting = user ? `Hello, ${user.profile.email}!` : `Hello, Stranger!`;
     document.getElementById('greeting').innerHTML = greeting;
@@ -14,14 +16,7 @@ function greetUser(user) {
     document.getElementById('logout').hidden = !user;
 }
 
-const mgr = new Oidc.UserManager(oidcConfig);
+document.getElementById('login').addEventListener("click", login, false);
+document.getElementById('logout').addEventListener("click", logout, false);
 
 mgr.getUser().then(greetUser).catch(console.error);
-
-function login() {
-    mgr.signinRedirect();
-}
-
-function logout() {
-    mgr.signoutRedirect();
-}
