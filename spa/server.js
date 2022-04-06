2f07c8a573076ccb12c599216b8224a58b547bc5
Slawek Zachcial <slawek.zachcial@gmail.com>
2022-04-06 22:10:30 +0200

    Fix Auth0 logout issue

--- spa/server.js
+++ spa/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const path = require('path')
+const axios = require('axios').default
 
 const app = express()
 const port = 3000
@@ -9,17 +10,22 @@ const port = 3000
 const static = express.static(path.join(__dirname, 'public'))
 app.use(static)
 
-app.get('/oidc-config.js', (req, res) => {
+app.get('/oidc-config.js', async (req, res) => {
+    // This is a hack to make Logout work with Auth0 which does not provide end_session_endpoint property in its metadata
+    const metadata = (await axios.get(`${process.env['OIDC_PROVIDER_URL']}.well-known/openid-configuration`)).data
+    metadata.end_session_endpoint = `${process.env['OIDC_PROVIDER_URL']}v2/logout?returnTo=${encodeURIComponent(process.env['BASE_URL']+'/')}`
+
     const spaOidcConfig = JSON.stringify({
-        authority: process.env['ISSUER_BASE_URL'],
+        authority: process.env['OIDC_PROVIDER_URL'],
         client_id: process.env['SPA_CLIENT_ID'],
         redirect_uri: `${process.env['BASE_URL']}/callback.html`,
-        post_logout_redirect_uri: `${process.env['BASE_URL']}/`,
+        // post_logout_redirect_uri: `${process.env['BASE_URL']}/`,
         response_type: 'code',
-        scope: "openid profile email"
+        scope: "openid profile email",
+        metadata: metadata
     })
     const awsOidcConfig = JSON.stringify({
-        authority: process.env['ISSUER_BASE_URL'],
+        authority: process.env['OIDC_PROVIDER_URL'],
         client_id: process.env['AWS_CLIENT_ID'],
         silent_redirect_uri: `${process.env['BASE_URL']}/callback-silent.html`,
         post_logout_redirect_uri: `${process.env['BASE_URL']}/`,
